9cda185acd452dde0e6cacb043f344851fdf68ef
om <omejjigiri99@gmail.com>
2024-11-11 08:47:57 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -10,28 +10,28 @@ def call() {
         stage('Compile') {
             echo 'Compiling the source code...'
             // Example compilation command (replace with actual command for your project)
-            sh './compile.sh' // Or `mvn compile`, `make`, etc.
+            
         }
 
         // Stage to run tests
         stage('Test') {
             echo 'Running tests...'
             // Example test command (replace with your testing framework)
-            sh './run-tests.sh' // Or `mvn test`, `npm test`, etc.
+            // Or `mvn test`, `npm test`, etc.
         }
 
         // Stage to build the artifact/package
         stage('Build') {
             echo 'Building the package...'
             // Example build command
-            sh './build.sh' // Or `mvn package`, `gradle build`, etc.
+             // Or `mvn package`, `gradle build`, etc.
         }
 
         // Stage to release/deploy the build
         stage('Release') {
             echo 'Releasing the build...'
             // Example release or deployment script
-            sh './release.sh' // Or `scp`, `kubectl apply`, etc.
+             // Or `scp`, `kubectl apply`, etc.
         }
     }
 }
