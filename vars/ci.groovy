98bde578d2fcd3c03763498a3039952742410209
om <omejjigiri99@gmail.com>
2024-11-11 09:16:27 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -15,7 +15,7 @@ def call() {
             
         }
 
-    } else if { (BRANCH_NAME ==~ "PR.*")
+    } else if { (BRANCH_NAME ==~ 'PR.*')
 
       sh'env'   
         // Stage to check out the source code from the repository
