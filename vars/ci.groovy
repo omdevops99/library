a973279a18bf73d1c65668488d153938a4d2c37d
om <omejjigiri99@gmail.com>
2024-11-13 17:05:30 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -37,7 +37,8 @@ def call() {
          sh 'sonar-scanner -Dsonar.host.url=http://172.31.37.111:9000 \
          -Dsonar.token=squ_d76c2ac190d03e20d46a4b26a8d0aba11e00cf87 \
          -Dsonar.projectKey=${repo_name}.git \
-         -Dsonar.exclusions=node_modules/**'   
+         -Dsonar.exclusions=node_modules/** \
+         -Dsonar.qualitygate.wait=true'  
 }    
         
         }     
