780795fa1fc7c406d1d3643dae5ef92f9fe949e1
om <omejjigiri99@gmail.com>
2024-11-13 17:17:30 +0530

    c

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -33,15 +33,15 @@ def call() {
              // Or `scp`, `kubectl apply`, etc.
         } 
 
-        stage('code quality') {
-          {
-         sh 'sonar-scanner -Dsonar.host.url=http://172.31.37.111:9000 \
-         -Dsonar.token=squ_d76c2ac190d03e20d46a4b26a8d0aba11e00cf87 \
-         -Dsonar.projectKey=${repo_name}.git \
-         -Dsonar.exclusions=node_modules/** \
-         -Dsonar.qualitygate.wait=true' 
-
-}    
+        stage('Code Quality') {
+           sh ''' 
+           sonar-scanner -Dsonar.host.url=http://172.31.37.111:9000 \
+                   -Dsonar.token=${sonar_token} \
+                   -Dsonar.projectKey=${repo_name}.git \
+                   -Dsonar.exclusions=node_modules/** \
+                   -Dsonar.qualitygate.wait=true
+    '''
+}  
         
         }     
         } else if(env.branch_name == "main"){
